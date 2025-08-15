99384cc0fe8b1d5e2f19083ffa925a28ed5b0744
Helg <Helg@helg.ru>
2025-08-15 10:37:35 +0300

    +

--- ImageTestArtifact/src/main/java/Application/ImageCreator.java
+++ ImageTestArtifact/src/main/java/Application/ImageCreator.java
@@ -1,42 +1,50 @@
 package Application;
 
+/*
 import javafx.scene.Node;
 import javafx.scene.canvas.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
+*/
 
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class ImageCreator {
 	public static BufferedImage CreateImage() {
 		BufferedImage bufferedImage = null;
-		
-		Platform.runLater(() -> {		
-		
-		Canvas canvas = new Canvas(416,88);
-		final GraphicsContext gc = canvas.getGraphicsContext2D();
-		gc.drawImage(GetActionDBImage("VMS","12"), 0, 0);
-		
-		
-		WritableImage writableImage = canvas.snapshot(null, null);
-		bufferedImage = SwingFXUtils.fromFXImage(writableImage, null);
-		//ImageIO.write(bufferedImage, format, outputFile)
-		
+
+		Platform.runLater(() -> {
+
+			Canvas canvas = new Canvas(416, 88);
+			final GraphicsContext gc = canvas.getGraphicsContext2D();
+			gc.drawImage(GetActionDBImage("VMS", "12"), 0, 0);
+
+			WritableImage writableImage = canvas.snapshot(null, null);
+			bufferedImage = SwingFXUtils.fromFXImage(writableImage, null);
+			// ImageIO.write(bufferedImage, format, outputFile)
+
 		});
 		return bufferedImage;
 	}
-	
-	
-	private static Image GetActionDBImage(String deviceType, String imageCode) {
-     Image result = null;
+
+	/***
+	 * Получение java.awt.Image из БД Actions
+	 * вероятно только растровый
+	 * @param deviceType
+	 * @param imageCode
+	 * @return
+	 */
+	private static java.awt.Image GetActionDBImage(String deviceType, String imageCode) {
+	 java.awt.Image result = null;
 	 Connection c = null;
      PreparedStatement stmt = null;
      
@@ -51,7 +59,7 @@ public class ImageCreator {
 	     stmt.setString(2, imageCode);
 	     ResultSet resultSet =  stmt.executeQuery();
 	     resultSet.next();
-	     result = new Image(resultSet.getBinaryStream("sign_file"));
+	     result = ImageIO.read(resultSet.getBinaryStream("sign_file")); 
 	     stmt.close();
 	     c.close();
      }
